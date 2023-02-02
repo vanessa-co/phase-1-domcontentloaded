55fdad18dfa1479a0cea87adc02096b02b4b45c9
Vanessa <vanessa.co403@gmail.com>
2023-02-01 20:05:06 -0500

    dom contest loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('page has loaded');
+    let text = document.getElementById('text').textContent = "This is really cool!";
+});
+
